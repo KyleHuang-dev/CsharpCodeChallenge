1da145240b796cdbb1891baae8f620327df073e7
your_github_username <kyle.huang.dev@gmail.com>
2022-07-20 09:42:58 -0600

    Create the greeting email for customers

---
+++ email_generator_164.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace email_generator_164
+{
+	class Program
+	{
+
+
+
+		public static void GreetCustomer(string name)
+		{
+			Console.WriteLine("Hi " + name + "!");
+			Console.WriteLine("Thank you very much for shopping with us.");
+			Console.WriteLine("Our shop usually keeps special offers for customers like you; " + name + ".");
+			Console.WriteLine("So " + name + ", do you want to explore your offer?");
+		}
+
+		static void Main(string[] args)
+		{
+
+			GreetCustomer("Alex");
+			Console.WriteLine();
+			GreetCustomer("Susan");
+			Console.WriteLine();
+			GreetCustomer("Ken");
+			Console.WriteLine();
+			Console.ReadLine();
+		}
+	}
+}
+
